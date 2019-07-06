4fe9a3f3550b5877cbce2fd772845c3fe52511af
ShivanshKeshari <40907270+ShivanshKeshari@users.noreply.github.com>
2019-07-07 01:58:27 +0530

    login for user

---
+++ DECODERS/login.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import sqlite3
+from tkinter import ttk, messagebox
+
+def login():
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password__login_entry = Entry(login_screen, textvariable=password_verify, show='*')
+    password__login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command=login_verification).pack()
